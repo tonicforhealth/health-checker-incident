d46247d1b5077c5647c9250b228da5c160d0f5d6
Dmitry Gopkalo <drefixs@gmail.com>
2016-03-12 00:21:51 +0200

    - fix route

--- src/IncidentBundle/Controller/IncidentController.php
+++ src/IncidentBundle/Controller/IncidentController.php
@@ -73,7 +73,7 @@ class IncidentController extends Controller
     }
 
     /**
-     * @Route("*")
+     * @Route("/")
      * @Method({"PUT"})
      * @param Request $request
      * @return Response
